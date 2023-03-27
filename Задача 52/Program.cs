e4203ca32b8767827c5b2c052cedc38a3f31d05c
Evgenii <w1djer@mail.ru>
2023-03-27 22:27:47 +0300

    Задача 52

---
+++ Задача 52/Program.cs
@@ -0,0 +1,28 @@
+﻿void InputMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 11);
+            Console.Write($"{matrix[i, j]} \t");
+        }
+        Console.WriteLine();
+    }
+}
+void Release(int[,] matrix)
+{
+   double sum = 0;
+   for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            sum += matrix[i,j];
+        }
+        Console.WriteLine($"Среднее арифметическое столбца {j+1} = {sum/4}");
+        sum = 0;
+    }
+}
+int[,] matrix = new int[4, 5];
+InputMatrix(matrix);
+Release(matrix);
